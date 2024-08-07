0a1ab1c7e11b692739f4b84da2af3244c2623f83
hino <asonehino@naver.com>
2024-08-07 19:53:04 +0900

    gyumin

--- test/src/App.js
+++ test/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'John Doe';
+  const naver = {
+    name: "Naver",
+    url: "https://naver.com",
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello 
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{
+        color: "red",
+        backgroundColor: "yellow",
+        padding: "20px",
+        borderRadius: "10px",
+        fontFamily: "Arial, sans-serif",
+        fontSize: "24px"
+      }}
+      >
+        welcome to React, {name}!
+        <a href = {naver.url}>{naver.name}</a>
+      </h1>
     </div>
   );
 }
