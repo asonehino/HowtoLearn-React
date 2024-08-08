b8c9e0f6aa006a0786719c4673f0a240ad2c4053
hino <asonehino@naver.com>
2024-08-08 15:19:20 +0900

    component

--- test/src/App.js
+++ test/src/App.js
@@ -1,25 +1,13 @@
-import './App.css';
+import "./App.css";
+import Welcome from "./component/Welcome";
+import Hello from "./component/Hello";
+import styles from "./App.module.css"
 
 function App() {
-  const name = 'John Doe';
-  const naver = {
-    name: "Naver",
-    url: "https://naver.com",
-  };
   return (
     <div className="App">
-      <h1 style={{
-        color: "red",
-        backgroundColor: "yellow",
-        padding: "20px",
-        borderRadius: "10px",
-        fontFamily: "Arial, sans-serif",
-        fontSize: "24px"
-      }}
-      >
-        welcome to React, {name}!
-        <a href = {naver.url}>{naver.name}</a>
-      </h1>
+      <Hello/>
+      <div className = {styles.box}>App</div>
     </div>
   );
 }
